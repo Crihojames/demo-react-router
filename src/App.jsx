3d2083f18ae21a0f2d371f78754ff07bd5aa9909
Crihojames <crihojames@gmail.com>
2023-11-30 12:43:19 +0100

    creat the Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductList from "./pages/ProductsList";
+import SingleProduct from "./pages/SingleProduct";
+import NavBar from "./Components/NavBar";
+
 
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+      <BrowserRouter>
+      <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/products" element={<Products/>}>
+            <Route index element={<ProductList />} />
+            <Route path=":id" element={<SingleProduct />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 };
 
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,13 @@
+import {Outlet} from "react-router-dom"
+
+
 
 const Products = () => {
     return (
-        <div>
+        <>
             <h1>Products</h1>
-        </div>
+            <Outlet />
+        </>
     );
 };
 
