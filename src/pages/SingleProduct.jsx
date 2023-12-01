06e53d0f9a95fd161e3fb340ca4d63350e2772f5
Crihojames <crihojames@gmail.com>
2023-12-01 12:11:09 +0100

    affiche les liste des medicament

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,8 +1,12 @@
+import ProductPrevieuw from "../Components/ProductPrevieuw";
+import { useParams } from "react-router-dom";
 
 const SingleProduct = () => {
+    const { id } = useParams()
     return (
         <div>
-            <h1>SingleProduct</h1>
+            <h1>Single Product {id}</h1>
+            <ProductPrevieuw />
         </div>
     );
 };
